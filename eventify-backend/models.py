e1a5fcfe7befa69cd73ac6217d5ab35f4bfdb521
IrineCKimeto <irenekimetoc@gmail.com>
2025-02-20 09:48:48 +0300

    add my models

---
+++ eventify-backend/models.py
@@ -0,0 +1,34 @@
+from app import db
+from datetime import datetime
+import uuid
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True,nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(200), nullable=False,default = "user")
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    location = db.Column(db.String(200), nullable=False)
+    ticket_price = db.Column(db.Float, nullable=False)
+    available_tickets = db.Column(db.Integer,nullable=False,default=50)
+    
+
+class Ticket(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey("event.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    purchase_date = db.Column(db.DateTime, default=datetime.now)
+    status = db.Column(db.String(20), default="confirmed")
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    ticket_id = db.Column(db.Integer, db.ForeignKey("ticket.id"), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(50), default="pending")
+    transaction_id = db.Column(db.String(30),unique=True,nullable=False,default=lambda: str(uuid.uuid4()))
