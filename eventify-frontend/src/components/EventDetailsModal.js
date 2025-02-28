7727fa02e4cc24bda58037c4e8c1820ac83dcc8f
Lybrook <omoshlybrook@gmail.com>
2025-02-28 18:03:02 +0300

    add loading state to EventDetailsModal component

--- eventify-frontend/src/components/EventDetailsModal.js
+++ eventify-frontend/src/components/EventDetailsModal.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function EventDetailsModal({ event, onClose }) {
+  if (!event) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white rounded-lg p-8 max-w-2xl w-full mx-4">
